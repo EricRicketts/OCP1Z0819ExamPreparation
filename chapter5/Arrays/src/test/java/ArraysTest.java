2a165b3342d6ae6a0ef7824fd202b8b64f0ac51b
EricRicketts <eric_ricketts@me.com>
2022-01-12 08:37:46 -0500

    OCP 1Z0-819 chapter 5, Core Java APIs, Arrays

--- chapter5/Arrays/src/test/java/ArraysTest.java
+++ chapter5/Arrays/src/test/java/ArraysTest.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,4 +13,47 @@ public class ArraysTest {
         }
     }
 
+    @Test
+    public void testSortArrays() {
+        int[] arr1 = {6, 9, 1};
+        char[] arr2 = {'c', 'a', 'b'};
+        int[] expectedInts = new int[]{1, 6, 9};
+        char[] expectedChars = new char[]{'a', 'b', 'c'};
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        assertArrayEquals(expectedInts, arr1);
+        assertArrayEquals(expectedChars, arr2);
+    }
+
+    @Test
+    public void testCompareNumericArrays() {
+        int[] arr1 = {1, 2, 3, 4};
+        int[] arr2 = {1, 2, 3, 4};
+        int[] arr3 = {1, 2, 3, 4, 5};
+        int[] arr4 = {1, 2, 3};
+
+        assertTrue(Arrays.compare(arr1, arr2) == 0);
+        assertTrue(Arrays.compare(arr1, arr3) < 0);
+        assertTrue(Arrays.compare(arr2, arr4) > 0);
+    }
+
+    @Test
+    public void testCompareStringsAndCharacters() {
+        char[] arr1 = {'a'};
+        char[] arr2 = {'A'};
+        String[] arr3 = {"a"};
+        String[] arr4 = {"aa"};
+
+        assertTrue(Arrays.compare(arr1, arr2) > 0);
+        assertTrue(Arrays.compare(arr3, arr4) < 0);
+    }
+
+    @Test
+    public void testArrayMismatch() {
+        int[] arr1 = {1, 3, 4};
+        int[] arr2 = {1, 3, 5};
+
+        assertEquals(2, Arrays.mismatch(arr1, arr2));
+    }
 }
